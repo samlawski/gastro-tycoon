a1d9b7d72e6e8eb56ebfa7871382b8413535a48a
Sam <sam.bogu@gmail.com>
2018-05-21 16:11:54 +0000

    make shuffle immutable

--- gastro_tycoon/functions/helper.js
+++ gastro_tycoon/functions/helper.js
@@ -1,5 +1,5 @@
 module.exports = {
   randomIndexOfArray: array => Math.floor(Math.random() * array.length),
   randomOf: array => array[Math.floor(Math.random() * array.length)],
-  shuffle: array => array.sort(() => Math.random() - 0.5)
+  shuffle: array => array.slice().sort(() => Math.random() - 0.5)
 }
